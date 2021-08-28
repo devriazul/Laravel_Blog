0d3651e752756a2b3c294b422b90a9fbd495675b
devriazul <riazul56@gmail.com>
2021-08-28 09:02:20 +0600

    Blade templating

--- routes/web.php
+++ routes/web.php
@@ -6,55 +6,60 @@ use App\Http\Controllers\SiteController;
 use Illuminate\Routing\RouteGroup;
 use App\Http\Controllers\MyController;
 use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\AboutController;
 
-//Simple route
+// //Simple route
 
-Route::get('/', [SiteController::class, 'home']);
+// Route::get('/', [SiteController::class, 'home']);
 
-Route::get('/contact', [SiteController::class, 'contact']);
+// Route::get('/contact', [SiteController::class, 'contact']);
 
-Route::get('/about', [SiteController::class, 'about']);
+// Route::get('/about', [SiteController::class, 'about']);
 
-// Parameter
+// // Parameter
 
-// Route::get('/{profileName}', [SiteController::class, 'profileName']);
+// // Route::get('/{profileName}', [SiteController::class, 'profileName']);
 
-// Multi parameter route
+// // Multi parameter route
 
-// Route::get('/{firstName}/{middleName}/{lastName}', [SiteController::class, 'myName']);
+// // Route::get('/{firstName}/{middleName}/{lastName}', [SiteController::class, 'myName']);
 
-// Group Route
+// // Group Route
 
-Route::group(['prefix' => 'accounts'], function () {
-
-
-
-    Route::get('/profile', function () {
-        return "Profile";
-    });
-
-    Route::get('/login', function () {
-        return "Login";
-    });
-
-    Route::get('/signup', function () {
-        return "SignUp";
-    });
-    Route::get('/update', function () {
-        return "Update";
-    });
-});
-
-//Single action controller
-
-Route::get('/single', MyController::class);
-
-//Resource Controller
+// Route::group(['prefix' => 'accounts'], function () {
 
 
-Route::resource('/photos', PhotoController::class);
 
-//Custom Controller
+//     Route::get('/profile', function () {
+//         return "Profile";
+//     });
 
-Route::get('/love', [HomeController::class, 'myself']);
-Route::get('/loveu', [HomeController::class, 'yourself']);
+//     Route::get('/login', function () {
+//         return "Login";
+//     });
+
+//     Route::get('/signup', function () {
+//         return "SignUp";
+//     });
+//     Route::get('/update', function () {
+//         return "Update";
+//     });
+// });
+
+// //Single action controller
+
+// Route::get('/single', MyController::class);
+
+// //Resource Controller
+
+
+// Route::resource('/photos', PhotoController::class);
+
+// //Custom Controller
+
+// Route::get('/love', [HomeController::class, 'myself']);
+// Route::get('/loveu', [HomeController::class, 'yourself']);
+
+// //Blade making
+
+Route::get('/about', [AboutController::class, 'about']);
