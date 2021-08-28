e3ae85e0fdcf0a8c013e4f87bdfa8dff0fdabb20
devriazul <riazul56@gmail.com>
2021-08-28 10:45:01 +0600

    looping in laravel

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -1,3 +1,5 @@
-<h1>New line</h1>
-{!!$NewHeadLinekey!!}
-{{$NewHeadLinekey}}
+@for ($i=0; $i<10; $i++)
+
+<img style="width: 190px;" src="https://scontent.fdac24-1.fna.fbcdn.net/v/t1.6435-9/197885147_4209012089155700_2010199435701144306_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=fQdnmPnyB90AX9S0b7_&_nc_ht=scontent.fdac24-1.fna&oh=288b48d1b041809f57ed6163dd36a880&oe=614DDA3A">
+
+@endfor
